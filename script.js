c57698f8f105cee6e996da1867656e9a612979ce
Michael Henrique Souza Franco <122502158+souza-franco@users.noreply.github.com>
2023-12-19 16:15:53 -0400

    a

---
+++ script.js
@@ -0,0 +1,26 @@
+const campo1 = document.querySelector("#campo1");
+const campo2 = document.querySelector("#campo2");
+const seletor = document.querySelector("#operacao");
+const botao = document.querySelector("#igual");
+let resposta = document.querySelector("#resposta");
+
+botao.addEventListener("click",calcular);
+
+function calcular(){
+    const valor1 = parseInt(campo1.value);
+    const valor2 = parseInt(campo2.value);
+    const operacao = seletor.value;
+
+    if(operacao==="somar")
+        resposta.innerHTML = valor1 + valor2;
+
+    if(operacao==="subtrair")
+        resposta.innerHTML = valor1 - valor2;
+
+    if(operacao==="multiplicar")
+        resposta.innerHTML = valor1 * valor2;
+
+    if(operacao==="dividir")
+        resposta.innerHTML = valor1 / valor2;
+
+}
